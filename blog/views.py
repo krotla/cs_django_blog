87cf137b52b7e40df89c7a2ab0a54ee29aa907dc
krotla <kkornel@o2.pl>
2025-11-17 21:52:08 +0100

    feat: Add post DetailView. Use class views for ListView and DetailView.

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from .models import Post
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+class PostDetailView(DetailView):
+    model = Post
 
 def home(request):
     context = {
